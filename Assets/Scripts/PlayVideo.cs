e8d2d63c7240d63e9c9e25578929b88edc5f3460
uzair332 <uzair332@gmail.com>
2022-11-03 14:56:46 +0100

    Many changes to level 1

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer, timeToStop);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+
+        if (player.gameObject.tag == "Player")
+        {
+            videoPlayer.SetActive(true);
+            Destroy(videoPlayer, timeToStop);
+        }
+    }
+}
